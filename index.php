0e635ec363d34e8e9b773587d5ffd2d91cb3bf68
Sangit Shrestha <63590703+30370657@users.noreply.github.com>
2021-07-20 21:40:30 +1000

    Basic layout

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+    
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/css/style.css">
+</head>
+<body>
+    <div class="header"> 
+        <div class="logo">
+            <img class="img" src="media/image/logo.jpg" alt="Logo">
+
+        </div>
+        <div class="text">
+            CareerGro Australia
+            Plan for better future
+            Educational Consultant
+
+        </div>
+    </div>
+
+    <div class="navi">
+        Navi
+
+    </div>
+    <div class="image">
+        Img
+
+    </div>
+    <div class="text">
+        text
+
+    </div>
+    <div class="secImg">
+        img
+
+    </div>
+    <div class="foot">
+
+        <div class="text">
+            text
+
+        </div>
+        <div class="img">
+            img
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
